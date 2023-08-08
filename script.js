8a6ba64e172467fa64d37b55819f5fc702be9510
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-08-08 12:44:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function threeSum(arr, target) {
   //your code here
+	  let closestSum = Infinity;
+    
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                let sum = nums[i] + nums[j] + nums[k];
+                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                    closestSum = sum;
+                }
+            }
+        }
+    }
+    
+    return closestSum;
 }
+
 
 module.exports = threeSum;
