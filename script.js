ba4b1b52de9c7f63bd30aef23dd39c177203aced
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-08-08 12:45:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function threeSum(arr, target) {
   //your code here
 	  let closestSum = Infinity;
     
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            for (let k = j + 1; k < nums.length; k++) {
-                let sum = nums[i] + nums[j] + nums[k];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            for (let k = j + 1; k < arr.length; k++) {
+                let sum = arr[i] + arr[j] + arr[k];
                 if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
                     closestSum = sum;
                 }
